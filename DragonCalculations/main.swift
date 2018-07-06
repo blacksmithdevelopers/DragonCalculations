77a0a783b4420bc6564eaa1446abd2f99cfa17d1
Matthew Elmore <elmo5676@gmail.com>
2018-07-05 21:51:28 -0700

    Initial Commit

---
+++ DragonCalculations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DragonCalculations
+//
+//  Created by Matthew Elmore on 7/5/18.
+//  Copyright © 2018 Matthew Elmore. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
